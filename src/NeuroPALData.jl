d22062390cafc8d1e97c6fe99c226fc219aa503e
Jungsoo Kim <jungsu93@gmail.com>
2022-12-22 16:36:54 -0500

    add functions from the nb

--- src/NeuroPALData.jl
+++ src/NeuroPALData.jl
@@ -5,6 +5,7 @@ using DelimitedFiles, XLSX
 include("reference.jl")
 include("import.jl")
 include("match.jl")
+include("class.jl")
 
 export invert_left_right,
     invert_dorsal_ventral,
@@ -13,6 +14,12 @@ export invert_left_right,
     get_neuron_roi,
     import_neuropal_label,
     # match.jl
-    match_roi
+    match_roi,
+    # class.jl
+    get_list_class,
+    get_list_class_dv,
+    generate_list_class_custom_order,
+    get_label_class,
+    get_list_match_dict
 
 end # module
